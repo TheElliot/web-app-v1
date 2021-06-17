c2e16c1b52b20da284259f7eba016e89781871be
TheElliot <61428709+TheElliot@users.noreply.github.com>
2021-06-17 10:49:46 -0500

    Add files via upload

---
+++ web-app.js
@@ -0,0 +1,29 @@
+// web-app.js JavaScript
+// don't forget to validate at https://jslint.com
+
+/*jslint devel: true, browser: true */
+
+// self-executing "global" anonymous function
+// it's here to keep variable and function scope
+// contained within our script
+(function () {
+
+    // use strict enforces more rules
+    // rules make us better programmers
+    "use strict";
+
+    // === === === === === === === === === === === === ===
+    // === === === ===   global variables  === === === ===
+    // === === === ===      begin here     === === === ===
+    // === === === === === === === === === === === === ===
+
+   
+
+    // === === === === === === === === === === === === ===
+    // === === === ===   global functions  === === === ===
+    // === === === ===      begin here     === === === ===
+    // === === === === === === === === === === === === ===
+
+ 
+
+}());
